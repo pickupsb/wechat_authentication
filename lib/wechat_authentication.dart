9710f88136bec2bd6788c1ad49ef034e6f13251a
pickupsb <springemi61@gmail.com>
2026-03-27 11:26:17 +0800

    fix\

--- lib/wechat_authentication.dart
+++ lib/wechat_authentication.dart
@@ -4,8 +4,13 @@ import 'package:wechat_authentication/wechat_authentication_method_channel.dart'
 import 'wechat_authentication_platform_interface.dart';
 
 class WechatAuthentication {
-  Future<String?> getPlatformVersion() {
-    return WechatAuthenticationPlatform.instance.getPlatformVersion();
+  // Future<String?> getPlatformVersion() {
+  //   return WechatAuthenticationPlatform.instance.getPlatformVersion();
+  // }
+  Future<void> init(String appid) {
+    return WechatAuthenticationPlatform.instance.init(appid);
   }
-
+  Future<String?> authenticate(String token) {
+    return WechatAuthenticationPlatform.instance.authenticate(token);
+  }
 }
